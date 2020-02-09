fa4b42da1d2899d1637e21d58b2006bd7d14ac3d
piedema <60652568+piedema@users.noreply.github.com>
2020-02-09 23:54:14 +0100

    simplefied testing script

--- test.js
+++ test.js
@@ -1,23 +1,12 @@
-let SMA = require('./trading-indicators')({name:'SMA', periods:5});
-SMA.update({ value:90.91 });
-SMA.update({ value:90.83 });
-SMA.update({ value:90.28 });
-SMA.update({ value:90.36 });
-SMA.update({ value:90.90 });
-SMA.update({ value:91.04 });
-SMA.update({ value:90.66 });
-SMA.update({ value:90.78 });
+
+let testvalues = [ 90.91, 90.83, 90.28, 90.36, 90.90, 91.04, 90.66, 90.78 ];
+
+let SMA = require('./trading-indicators')('SMA', 5);
+for(let i = 0; i < testvalues.length; i++) SMA.update({ value:testvalues[i] });
 console.log('SMAs: ', SMA.get()); // should be [ 90.75, 90.65, 90.68, 90.66 ]
 console.log('SMA: ', SMA.getLast()); // should be 90.75
 
-let WMA = require('./trading-indicators')({name:'WMA', periods:5});
-WMA.update({ value:90.91 });
-WMA.update({ value:90.83 });
-WMA.update({ value:90.28 });
-WMA.update({ value:90.36 });
-WMA.update({ value:90.90 });
-WMA.update({ value:91.04 });
-WMA.update({ value:90.66 });
-WMA.update({ value:90.78 });
+let WMA = require('./trading-indicators')('WMA', 5);
+for(let i = 0; i < testvalues.length; i++) WMA.update({ value:testvalues[i] });
 console.log('WMAs: ', WMA.get()); // should be [ 90.79, 90.74, 90.75, 90.62 ]
 console.log('WMA: ', WMA.getLast()); // should be 90.79
